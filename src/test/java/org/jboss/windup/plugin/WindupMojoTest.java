e75ecd09d0809d955091b428645af93ae9904c6d
Fedora <fedora@fedora-slave-release-55.localdomain>
2019-03-14 20:05:20 +0000

    Updated version.windupcore property to 4.2.1-SNAPSHOT

--- src/test/java/org/jboss/windup/plugin/WindupMojoTest.java
+++ src/test/java/org/jboss/windup/plugin/WindupMojoTest.java
@@ -76,6 +76,6 @@ public class WindupMojoTest extends AbstractMojoTestCase
         mojo2.execute();
 
 
-        assertEquals(mojo2.getWindupVersion(), "4.2.1.Alpha2");
+        assertEquals(mojo2.getWindupVersion(), "4.2.1-SNAPSHOT");
     }
 }
