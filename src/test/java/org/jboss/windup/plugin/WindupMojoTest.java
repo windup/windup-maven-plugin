2120ce5bad7c175f60bead68c22e70058bad5625
Mark Brophy <36955467+m-brophy@users.noreply.github.com>
2020-07-03 09:35:38 +0100

    version upgrade

--- src/test/java/org/jboss/windup/plugin/WindupMojoTest.java
+++ src/test/java/org/jboss/windup/plugin/WindupMojoTest.java
@@ -76,6 +76,6 @@ public class WindupMojoTest extends AbstractMojoTestCase
         mojo2.execute();
 
 
-        assertEquals(mojo2.getWindupVersion(), "4.3.2-SNAPSHOT");
+        assertEquals(mojo2.getWindupVersion(), "5.0.0-SNAPSHOT");
     }
 }
