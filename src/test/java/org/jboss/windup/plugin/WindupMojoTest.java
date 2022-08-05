3863f42c5bfdbc095f39e294a78b1cdb29922363
Windup <windup@users.noreply.github.com>
2022-08-05 16:21:15 +0000

    Back to development

--- src/test/java/org/jboss/windup/plugin/WindupMojoTest.java
+++ src/test/java/org/jboss/windup/plugin/WindupMojoTest.java
@@ -76,6 +76,6 @@ public class WindupMojoTest extends AbstractMojoTestCase
         mojo2.execute();
 
 
-        assertEquals(mojo2.getWindupVersion(), "6.0.0.Alpha1");
+        assertEquals(mojo2.getWindupVersion(), "6.0.0-SNAPSHOT");
     }
 }
