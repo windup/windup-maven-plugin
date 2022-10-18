7f11ce5eecdda19e7a35838b8dd5f528c609bc32
Windup <windup@users.noreply.github.com>
2022-10-18 12:35:50 +0000

    Preparing for release

--- windup-plugin/src/test/java/org/jboss/windup/plugin/WindupMojoTest.java
+++ windup-plugin/src/test/java/org/jboss/windup/plugin/WindupMojoTest.java
@@ -76,6 +76,6 @@ public class WindupMojoTest extends AbstractMojoTestCase
         mojo2.execute();
 
 
-        assertEquals(mojo2.getWindupVersion(), "6.0.1-SNAPSHOT");
+        assertEquals(mojo2.getWindupVersion(), "6.1.0.Alpha1");
     }
 }
