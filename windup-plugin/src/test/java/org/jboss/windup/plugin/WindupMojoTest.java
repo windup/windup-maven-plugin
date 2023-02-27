bdf721794e5ba5522479de7d59873d27ef1b3e07
windup <windup@users.noreply.github.com>
2023-02-27 09:48:06 +0000

    🏁 Releasing version 6.1.3.Final
    Signed-off-by: windup <windup@users.noreply.github.com>

--- windup-plugin/src/test/java/org/jboss/windup/plugin/WindupMojoTest.java
+++ windup-plugin/src/test/java/org/jboss/windup/plugin/WindupMojoTest.java
@@ -76,6 +76,6 @@ public class WindupMojoTest extends AbstractMojoTestCase
         mojo2.execute();
 
 
-        assertEquals(mojo2.getWindupVersion(), "6.1.3-SNAPSHOT");
+        assertEquals(mojo2.getWindupVersion(), "6.1.3.Final");
     }
 }
