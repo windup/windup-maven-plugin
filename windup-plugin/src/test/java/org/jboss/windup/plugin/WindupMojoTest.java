49d56b9a08d25404ae71c5377a2d544e14762ec7
windup <windup@users.noreply.github.com>
2023-10-13 20:12:21 +0000

    🏁 Releasing version 6.3.2.Final
    Signed-off-by: windup <windup@users.noreply.github.com>

--- windup-plugin/src/test/java/org/jboss/windup/plugin/WindupMojoTest.java
+++ windup-plugin/src/test/java/org/jboss/windup/plugin/WindupMojoTest.java
@@ -93,7 +93,7 @@ public class WindupMojoTest extends AbstractMojoTestCase
         mojo2.execute();
 
 
-        assertEquals(mojo2.getWindupVersion(), "6.3.2-SNAPSHOT");
+        assertEquals(mojo2.getWindupVersion(), "6.3.2.Final");
         assertTrue("index.html report not found",
                 Paths.get("",
                                 ((Xpp3Dom) project.getBuild().getPlugins().get(0).getConfiguration()).getChild("outputDirectory").getValue(),
